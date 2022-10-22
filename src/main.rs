d0331fe52ee3d481f80778cbd5d12617f905387b
Niklas Mollenhauer <nikeee@outlook.com>
2022-10-22 14:33:42 +0200

    Read shared prefs from zip file

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
-use std::fs::read;
+use std::{fs::{read, File}, path::Path, io::{BufReader, Read}};
 
 mod key;
 mod prefs;
 
 // TODO: Error handling
 fn main() {
-    let shared_prefs = read("").unwrap();
+
+    let backup_path = Path::new("./backup.zip");
+    let backup_file = File::open(&backup_path).unwrap();
+    let backup_reader = BufReader::new(backup_file);
+
+    let mut zip_file = zip::ZipArchive::new(backup_reader).unwrap();
+
+    let mut sp = zip_file.by_name("StarMoneyPrefs").unwrap();
+    let mut shared_prefs = Vec::new();
+    sp.read_to_end(&mut shared_prefs).unwrap();
+
     let key_params = prefs::read_key_params_from_shared_preferences(&shared_prefs).unwrap();
     let sqlcipher_key = key::decrypt_key(&key_params, &"abc").unwrap();
 }
