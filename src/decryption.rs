a38289eab351e2dff0900ebfe685206b56b79b49
Niklas Mollenhauer <nikeee@outlook.com>
2022-10-22 17:26:45 +0200

    Add weird error handling

--- src/decryption.rs
+++ src/decryption.rs
@@ -3,33 +3,79 @@ use std::io::Read;
 use rusqlite::Connection;
 use tempfile::NamedTempFile;
 
-use crate::{prefs, key};
+use crate::{key, prefs};
 
-// TODO: Error handling
-pub fn decrypt_sqlcipher_key<T: Read>(shared_prefs: &mut T, password: &str) -> Option<String> {
+pub fn decrypt_sqlcipher_key<T: Read>(
+    shared_prefs: &mut T,
+    password: &str,
+) -> Result<String, DecryptionError> {
     let mut shared_prefs_buf = Vec::new();
-    shared_prefs.read_to_end(&mut shared_prefs_buf).unwrap();
+    shared_prefs
+        .read_to_end(&mut shared_prefs_buf)
+        .map_err(|_| "Unable read shared prefs")?;
 
-    let key_params = prefs::read_key_params_from_shared_preferences(&shared_prefs_buf).unwrap();
+    let key_params = prefs::read_key_params_from_shared_preferences(&shared_prefs_buf)
+        .ok_or(DecryptionError::new("Unable read shared prefs"))?;
 
-    key::decrypt_key(&key_params, &password).unwrap().into()
+    key::decrypt_key(&key_params, &password)
+        .ok_or(DecryptionError::new("Unable to decrypt SQLcipher key."))
+        .into()
 }
 
-pub fn decrypt_database_file_to<T: Read>(encrypted_database: &mut T, key: &str, out_file: &str) -> Option<()> {
-    let mut file = NamedTempFile::new().unwrap(); // TODO: Error handling
+pub fn decrypt_database_file_to<T: Read>(
+    encrypted_database: &mut T,
+    key: &str,
+    out_file: &str,
+) -> Result<(), DecryptionError> {
+    let mut file = NamedTempFile::new().map_err(|_| "Unable to create temporary file")?;
 
-    std::io::copy(encrypted_database, &mut file).unwrap();
+    std::io::copy(encrypted_database, &mut file)
+        .map_err(|_| "Unable to copy encrypted data to temporary file")?;
 
-    let conn = Connection::open(file.path()).unwrap(); // TODO: Error handling
+    let conn = Connection::open(file.path())
+        .map_err(|_| "Unable to open database connection to encrypted temporary file")?;
 
-    conn.pragma_update(None, "key", key).unwrap(); // TODO: Error handling
+    conn.pragma_update(None, "key", key)
+        .map_err(|_| "Could not set key for sqlcipher.")?;
 
     // https://stackoverflow.com/a/32571540
     // Empty key will disable encryption
-    conn.execute("ATTACH DATABASE ? AS plaintext KEY ''", &[out_file]).unwrap(); // TODO: Error handling
-    conn.query_row("SELECT sqlcipher_export('plaintext')", [], |_| Ok(())).unwrap(); // TODO: Error handling
-    conn.execute("DETACH DATABASE plaintext", []).unwrap(); // TODO: Error handling
+    conn.execute("ATTACH DATABASE ? AS plaintext KEY ''", &[out_file])
+        .map_err(|_| "Unable to attach output database")?;
 
-    file.close().unwrap(); // TODO: Error handling
-    Some(())
+    conn.query_row("SELECT sqlcipher_export('plaintext')", [], |_| Ok(()))
+        .map_err(|_| "Unable to export plaintext database")?;
+
+    conn.execute("DETACH DATABASE plaintext", []).map_err(|_| {
+        "Could not DETACH plaintext database. Plaintext data may be written to disk regardless."
+    })?;
+
+    file.close()
+        .map_err(|_| "Unable to close temporary file.")?;
+    Ok(())
+}
+
+#[derive(Debug, Clone)]
+pub struct DecryptionError {
+    pub message: String,
+}
+
+impl DecryptionError {
+    fn new(message: impl Into<String>) -> Self {
+        Self {
+            message: message.into(),
+        }
+    }
+}
+
+impl From<String> for DecryptionError {
+    fn from(str: String) -> Self {
+        Self::new(str)
+    }
+}
+
+impl From<&str> for DecryptionError {
+    fn from(str: &str) -> Self {
+        Self::new(str)
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -19,11 +19,13 @@ fn main() {
 
     let mut zip_file = zip::ZipArchive::new(backup_reader).unwrap();
 
-    let key = {
-        let mut sp = zip_file.by_name("StarMoneyPrefs").unwrap();
-        decryption::decrypt_sqlcipher_key(&mut sp, password).unwrap()
+    let key = match zip_file.by_name("StarMoneyPrefs") {
+        Err(_) => panic!("Could not find StarMoneyPrefs in backup. Most likely, the provided ZIP file is not a valid backup."),
+        Ok(mut sp) => decryption::decrypt_sqlcipher_key(&mut sp, password).unwrap_or_else(|e| panic!("{}", e.message))
     };
 
-    let mut encrypted_database = zip_file.by_name(DATABASE_PATH).unwrap();
-    decryption::decrypt_database_file_to(&mut encrypted_database, &key, &out_file);
+    let mut encrypted_database = zip_file.by_name(DATABASE_PATH).unwrap_or_else(|_| panic!("Could not find StarMoneyPrefs in backup. Most likely, the provided ZIP file is not a valid backup."));
+
+    decryption::decrypt_database_file_to(&mut encrypted_database, &key, &out_file)
+        .unwrap_or_else(|e| panic!("{}", e.message))
 }
